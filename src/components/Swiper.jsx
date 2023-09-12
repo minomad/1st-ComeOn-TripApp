32aa75eaf82a09f679edf1707180d834f5e24aa7
minomad <93kymins@gmail.com>
2023-09-12 16:08:08 +0900

    Feat: Swiper 추가 Swiper 이미지 및 아이콘 추가

---
+++ src/components/Swiper.jsx
@@ -0,0 +1,39 @@
+import { Children, useState } from 'react';
+import { motion } from 'framer-motion';
+
+function Swiper({ children, initial, animate, transition, className }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const childrenArray = Children.toArray(children);
+
+  const prev = () => {
+    setCurrentIndex((index) => (index === 0 ? childrenArray.length - 1 : index - 1));
+  };
+
+  const next = () => {
+    setCurrentIndex((index) => (index === childrenArray.length - 1 ? 0 : index + 1));
+  };
+
+  return (
+    <div className='relative'>
+      <motion.div
+        key={currentIndex}
+        initial={initial} //initial opacity: 1, x: -20, y: 0
+        animate={animate} //animate opacity: 1, x: 0, y: 0
+        transition={transition} //transition duration: 0.5
+        className={className}
+      >
+        {childrenArray[currentIndex]}
+      </motion.div>
+
+      <button onClick={prev} className='absolute left-2 top-1/2 -translate-y-5'>
+        <img src='/prev.svg' alt='이전' className='rounded-full shadow-white drop-shadow-md' />
+      </button>
+      <button onClick={next} className='absolute right-2 top-1/2 -translate-y-5'>
+        <img src='/next.svg' alt='다음' className='rounded-full shadow-white drop-shadow-md  ' />
+      </button>
+    </div>
+  );
+}
+
+export default Swiper;
