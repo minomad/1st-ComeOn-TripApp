ff2b5100322735fc73a4505916a38cb7b1413304
HYHYJ <jjangdaram@naver.com>
2023-09-13 09:24:38 +0900

    Feat: AroundPage 지도 경도위도 연결, 아이콘 추가, 목록의 호텔 상세페이지 연결
    AroundPage 지도 경도위도 연결, 아이콘 추가, 목록의 호텔 상세페이지 연결
    
    Resolves: #40

--- src/components/AroundList.jsx
+++ src/components/AroundList.jsx
@@ -1,8 +1,9 @@
 import { getPbImageURL } from '@/utils/getPbImageURL';
 import { numberWithComma } from '@/utils/numberWithComma';
-
-import Button from './Button'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button'
+
 
 function AroundList({data, selectCategory}) {
   let filterData;
@@ -31,48 +32,54 @@ function AroundList({data, selectCategory}) {
 
   return (
     <section className='relative'>
-    <h2 className='sr-only'>내 주변 숙소 목록</h2>
-    <Button type='button' className='ml-auto mr-3 mt-auto flex rounded-full text-[0.9rem] py-2 px-4 font-bold'
-      onClick={() => {setSelectOrder((e) => !e);}}
-    > {selectList}   <img src='/back.svg' alt='뒤로가기' className='rotate-[270deg] w-5 pt-0.5' />
-        </Button>
-    <div className={` absolute right-9 top-8 w-[4rem] text-center bg-white rounded-md shadow-md ${!selectOrder ? 'hidden' : ''}`}>
-          <ul>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
-          setselectList('추천순')}}>추천순</li>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
-          setselectList('인기순')}}>인기순</li>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
-          setselectList('최신순')}}>최신순</li>
-          </ul>
-    </div>
-    {filterData?.map((item) => (
-      <article key={item.id} className='h-[30rem] lg:h-[33rem] py-1.2   shadow-md  bg-white '>
-        <figure className='w-[100%] h-[57%] lg:h-[65%]  overflow-hidden mr-4 '>
-          <img 
-            src={getPbImageURL(item, 'img')} 
-            alt={item.title} 
-            className=' h-[120%] w-[110%]  cover object-cover' />
-          <figcaption className='sr-only'>{item.title} </figcaption>
-        </figure> 
-        {/* 호텔명, 별점, 가격 설명박스 */}
-        <div className='px-6 pt-3  '>
-          <h2 className='font-bold text-lg'>{item.title}</h2>
-          <img src='/star.svg' alt='별점' className='inline-block mb-[0.3rem] mr-[0.3rem] '/>
-          <span className='font-extrabold text-[0.9rem]'>{item.star} </span>
-          <span className='font-bold text-gray2 text-[0.9rem]'>&#183; {item.grade} </span>
+      <h2 className='sr-only'>내 주변 숙소 목록</h2>
+    <nav className='fixed bg-white pt-[6rem] top-0 left-0 right-0 max-w-3xl mx-auto'>
+      <Button type='button' className='ml-auto mr-3 mt-auto flex rounded-full text-[0.9rem] pt-2 pb-1 px-4 font-bold'
+        onClick={() => {setSelectOrder((e) => !e);}}
+      > {selectList}   <img src='/back.svg' alt='뒤로가기' className='rotate-[270deg] w-5 pt-0.5' />
+          </Button>
+      <div className={`absolute right-9 top-[8rem] w-[4rem] text-center bg-white rounded-md shadow-md ${!selectOrder ? 'hidden' : ''}`}>
+            <ul>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
+            setselectList('추천순')}}>추천순</li>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
+            setselectList('인기순')}}>인기순</li>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
+            setselectList('최신순')}}>최신순</li>
+            </ul>
+      </div>
+    </nav>
 
-          <div className=' flex justify-between py-4  '>
-            <span className='block text-[0.9rem] mr-3 text-gray2'>숙박: 15:00~
-            </span>
-              <span className='block text-[1.2rem] font-extrabold '>{numberWithComma(item.price)}원</span>
+    <div className='my-[8rem]'>
+      {filterData?.map((item) => (
+        <article key={item.id} className='h-[30rem] lg:h-[33rem] py-1.2   shadow-md  bg-white '>
+          <figure className='w-[100%] h-[57%] lg:h-[65%]  overflow-hidden mr-4 '>
+            <img
+              src={getPbImageURL(item, 'img')}
+              alt={item.title}
+              className=' h-[120%] w-[110%]  cover object-cover' />
+            <figcaption className='sr-only'>{item.title} </figcaption>
+          </figure>
+          {/* 호텔명, 별점, 가격 설명박스 */}
+          <div className='px-6 pt-3  '>
+            <h2 className='font-bold text-lg'>{item.title}</h2>
+            <img src='/star.svg' alt='별점' className='inline-block mb-[0.3rem] mr-[0.3rem] '/>
+            <span className='font-extrabold text-[0.9rem]'>{item.star} </span>
+            <span className='font-bold text-gray2 text-[0.9rem]'>&#183; {item.grade} </span>
+            <div className=' flex justify-between py-4  '>
+              <span className='block text-[0.9rem] mr-3 text-gray2'>숙박: 15:00~
+              </span>
+                <span className='block text-[1.2rem] font-extrabold '>{numberWithComma(item.price)}원</span>
+            </div>
+            <Link to={`hotel/${item.id}`}>
+              <Button type='button' className='ml-auto mr-0 mb-2 mt-auto flex rounded-full  bg-primary text-white font-medium  text-[1rem] shadow-md py-2 px-6 gap-2 '
+              > {'숙소 예약하기'}
+              </Button>
+            </Link>
           </div>
-          <Button type='button' className='ml-auto mr-0 mb-2 mt-auto flex rounded-full  bg-primary text-white font-medium  text-[1rem] shadow-md py-2 px-6 gap-2 '
-          > {'숙소 예약하기'}
-        </Button>
-        </div>
-      </article>
-      ))}
+        </article>
+        ))}
+    </div>
     
     </section>
   )
@@ -111,57 +118,62 @@ export function AroundLeisureList({data, selectCategory}) {
   return (
     <section className='relative'>
     <h2 className='sr-only'>내 주변 레저/티켓 목록</h2>
-    <Button 
-      type='button' 
-      className='ml-auto mr-3 mt-auto flex rounded-full  text-[0.9rem] py-2 px-4 font-bold'
-      onClick={() => {setSelectOrder((e) => !e);}}
-     > 
-      {selectList}   <img src='/back.svg' alt='뒤로가기' className='rotate-[270deg] w-5 pt-0.5'/>
-     </Button>
-    <div className={`absolute right-9 top-8 w-[4rem] text-center bg-white rounded-md shadow-md ${!selectOrder ? 'hidden' : ''}`}>
-          <ul>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem] ' onClick={() => {
-          setselectList('추천순')}}>추천순</li>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
-          setselectList('인기순')}}>인기순</li>
-            <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
-          setselectList('최신순')}}>최신순</li>
-          </ul>
+    <div className='fixed bg-white pt-[6rem] top-0 left-0 right-0 max-w-3xl mx-auto'>
+      <Button
+        type='button'
+        className='ml-auto mr-3 mt-auto flex rounded-full  text-[0.9rem] pt-2 pb-1 px-4 font-bold '
+        onClick={() => {setSelectOrder((e) => !e);}}
+       >
+        {selectList}   <img src='/back.svg' alt='뒤로가기' className='rotate-[270deg] w-5 pt-0.5'/>
+       </Button>
+      <div className={`absolute right-9 top-[8rem] w-[4rem] text-center bg-white rounded-md shadow-md ${!selectOrder ? 'hidden' : ''}`}>
+            <ul>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem] ' onClick={() => {
+            setselectList('추천순')}}>추천순</li>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
+            setselectList('인기순')}}>인기순</li>
+              <li className='hover:bg-lightPurple text-[0.9rem] py-[0.5rem]' onClick={() => {
+            setselectList('최신순')}}>최신순</li>
+            </ul>
+      </div>
     </div>
-    {filterData?.map((item) => (
-      <article key={item.id} className='h-[30rem] lg:h-[33rem] py-1.2   shadow-md  bg-white '>
-        <figure className='w-[100%] h-[57%] lg:h-[65%]  overflow-hidden mr-4 '>
-          <img 
-            src={getPbImageURL(item, 'main')} 
-            alt={item.title} 
-            className=' h-[120%] w-[110%]  cover object-cover' />
-          <figcaption className='sr-only'>{item.title} </figcaption>
-        </figure> 
-        {/* 호텔명, 별점, 가격 설명박스 */}
-        <div className='px-6 pt-3  '>
-          <h2 className='font-bold text-lg'>{item.title}</h2>
-          
-          
-
-          <div className=' flex justify-between py-2  '>
-            <span className='block '>
-            {item.label.map((label) => (
-                  <span
-                    key={label}
-                    className='mr-1 mt-1 rounded-[2px] border-[1px] border-[#e6e6e6] bg-[#f2f2f2] px-1 py-[2px] text-[11px]'
-                  >
-                    {label}
-                  </span>
-                ))}
-            </span>
-              <span className='block text-[1.2rem] font-extrabold '>{numberWithComma(item.price)}원</span>
+    <div className='my-[8rem] '>
+      {filterData?.map((item) => (
+        <article key={item.id} className='h-[30rem] lg:h-[33rem] py-1.2   shadow-md  bg-white '>
+          <figure className='w-[100%] h-[57%] lg:h-[65%]  overflow-hidden mr-4 '>
+            <img
+              src={getPbImageURL(item, 'main')}
+              alt={item.title}
+              className=' h-[120%] w-[110%]  cover object-cover' />
+            <figcaption className='sr-only'>{item.title} </figcaption>
+          </figure>
+          {/* 호텔명, 별점, 가격 설명박스 */}
+          <div className='px-6 pt-3  '>
+            <h2 className='font-bold text-lg'>{item.title}</h2>
+      
+      
+            <div className=' flex justify-between py-2  '>
+              <span className='block '>
+              {item.label.map((label) => (
+                    <span
+                      key={label}
+                      className='mr-1 mt-1 rounded-[2px] border-[1px] border-[#e6e6e6] bg-[#f2f2f2] px-1 py-[2px] text-[11px]'
+                    >
+                      {label}
+                    </span>
+                  ))}
+              </span>
+                <span className='block text-[1.2rem] font-extrabold '>{numberWithComma(item.price)}원</span>
+            </div>
+            <Link to={`LeisureDetail/${item.id}`}>
+              <Button type='button' className='ml-auto mr-0 mb-2 mt-auto flex rounded-full  bg-primary text-white font-medium  text-[1rem] shadow-md py-2 px-6 gap-2 '
+              > {'상품 선택하기'}
+              </Button>
+            </Link>
           </div>
-          <Button type='button' className='ml-auto mr-0 mb-2 mt-auto flex rounded-full  bg-primary text-white font-medium  text-[1rem] shadow-md py-2 px-6 gap-2 '
-          > {'상품 선택하기'}
-        </Button>
-        </div>
-      </article>
-      ))}
+        </article>
+        ))}
+    </div>
     
     </section>
   )
