d523c434397bbd56d312929e45a06e799af0c974
minomad <93kymins@gmail.com>
2023-09-18 21:05:58 +0900

    Fix: 메타태그수정

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function Hotel({ data, selectCategory }) {
-  const filteredData = data.filter((hotel) => {
-    return selectCategory === '전체' ? true : hotel.category === selectCategory;
+  const filterData = data.filter((hotel) => {
+    return selectCategory === '' ? true : hotel.category === selectCategory;
   });
-  
+
   return (
-    <div role='list' className='flex justify-center'>
-      <ul className='max-w-screen-lg:grid-cols-2 grid grid-cols-4 gap-4'>
-        {filteredData.map((item) => (
-          <li key={item.id} className='max-w-screen-lg:px-2 py-1 hover:scale-105'>
-            <Link to={`/hotel/${item.id}`} aria-label={`${item.title} 상세 정보로 이동`}>
+    <div role='ListWrapper' className='flex justify-center'>
+      <ul className='grid grid-cols-4 gap-4 max-[610px]:grid-cols-2'>
+        {filterData?.map((item) => (
+          <li key={item.id} className='py-1 hover:scale-105 max-[610px]:px-2 max-[360px]:px-0'>
+            <Link to={`/hotel/${item.id}`}>
               <img
                 src={getPbImageURL(item, 'img')}
                 alt={item.title}
                 className='h-32 w-36 rounded-lg'
               />
-              <div className='pt-1 font-semibold'>{item.title}</div>
-              <div className='text-gray-500 flex items-center gap-1 text-sm'>
+              <p className=' pt-1 font-semibold'>{item.title}</p>
+              <div className='flex items-center gap-1 text-sm text-gray2'>
                 <img src='/star.svg' alt='평점' className='h-4 w-4' />
                 <span>{item.star}</span>
                 <span>({item.count})</span>
               </div>
-              <div className='pt-1 text-right font-semibold text-primary'>
+              <p className='pt-1 text-right font-semibold text-primary'>
                 {numberWithComma(item.price)}원
-              </div>
+              </p>
             </Link>
           </li>
         ))}
