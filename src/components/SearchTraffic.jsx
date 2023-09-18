a308765e0266dbb805b53c31a8825783e88f14b6
HYHYJ <jjangdaram@naver.com>
2023-09-18 09:32:28 +0900

    Feat:  SearchPage 검색 기능 추가
    SearchPage 검색 기능 추가
    
    Related to: #67

--- src/components/SearchTraffic.jsx
+++ src/components/SearchTraffic.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import TrafficReserveButton from './TrafficReserveButton'
 
 function SearchTraffic() {
@@ -5,9 +6,15 @@ function SearchTraffic() {
     <section>
     <h3 className='sr-only'>교통/항공 검색</h3>
       <ul className='mx-auto w-[87%] md:w-[77%]'>
+      <Link to={'/install'}>
         <li><TrafficReserveButton src={'/leisure-bus.png'} alt={'고속버스'} /></li>
+      </Link>
+      <Link to={'/train'}>
         <li><TrafficReserveButton src={'/leisure-train.png'} alt={'기차'} /> </li>
+      </Link> 
+      <Link to={'/airline'}>
         <li><TrafficReserveButton src={'/leisure-airplane.png'} alt={'항공권'} /></li>
+      </Link> 
       </ul>
     </section>
   )
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,18 +1,37 @@
 import { useState } from 'react';
 import { Helmet } from 'react-helmet-async'
+import { useQuery } from '@tanstack/react-query';
+import { usePocketData } from '@/api/usePocketData';
 import Header from '@/components/Header';
 import HotelInfoCategory from '@/components/HotelInfoCategory';
 import SearchHotel from '@/components/SearchHotel';
 import SearchLeisure from '@/components/SearchLeisure';
 import SearchTraffic from '@/components/SearchTraffic';
+import Spinner from '@/components/Spinner';
 
 function SearchPage() {
   const info = ['숙소', '레저/티켓', '교통/항공'];
   const [selectCategory, setSelectCategory] = useState('숙소');
+  const { getListData } = usePocketData('hotel');
+  const {
+    data: hotelData,
+    isLoading,
+    isError,
+  } = useQuery(['hotelSearch'], () => getListData());
+
 
   const handleChangeCategory = (category) => {
     setSelectCategory(category);
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <div className='text-accent'>서버 에러 발생</div>;
+  }
+
   return (
     <>
      <Helmet>
@@ -28,7 +47,7 @@ function SearchPage() {
           selectCategory={selectCategory}
           handleChangeCategory={handleChangeCategory}
         />
-        {selectCategory === '숙소' && <SearchHotel/>}
+        {selectCategory === '숙소' && <SearchHotel data={hotelData}/>}
         {selectCategory === '레저/티켓' && <SearchLeisure/>}
         {selectCategory === '교통/항공' && <SearchTraffic/>}
         
