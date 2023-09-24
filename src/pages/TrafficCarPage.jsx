c4d52abbd665afc4d22f847a1538cc7e0fd0f5bf
minomad <93kymins@gmail.com>
2023-09-24 17:03:41 +0900

    Fix: 오타수정

--- src/pages/TrafficCarPage.jsx
+++ src/pages/TrafficCarPage.jsx
@@ -5,7 +5,8 @@ import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '@/components/Header';
-import LeisureCategory from '@components/LeisureCategory';
+import LeisureCategory from '@/components/LeisureCategory';
+
 function TrafficCarPage() {
   const [selectCategory, setSelectCategory] = useState('전체');
   const category = ['전체', '제주이용권', '내륙이용권'];
